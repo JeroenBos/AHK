535f0d10a34a7256315e8057c0f8f36f8b279c1e
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2022-07-11 13:40:59 +0200

    Show error if it occurs on startup

--- NoteWork/Program.cs
+++ NoteWork/Program.cs
@@ -11,7 +11,17 @@ namespace NoteWork
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            var form = NoteWorkForm.Create();
+
+            NoteWorkForm? form = null;
+            try
+            {
+                form = NoteWorkForm.Create();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
             if (form != null)
             {
                 Application.Run(form);
--- PrintscreenToTodo/Program.cs
+++ PrintscreenToTodo/Program.cs
@@ -11,9 +11,21 @@ namespace PrintscreenToTodo
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            var form = PrintscreenToTodoForm.Create();
+
+            PrintscreenToTodoForm? form = null;
+            try
+            {
+                form = PrintscreenToTodoForm.Create();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
             if (form != null)
+            {
                 Application.Run(form);
+            }
         }
     }
 }
