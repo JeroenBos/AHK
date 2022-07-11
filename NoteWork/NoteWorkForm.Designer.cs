23e4cf466f34fb69700d53baf9f883e42e3dccef
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2022-07-11 12:39:48 +0200

    Create NoteWork form, initial version

---
+++ NoteWork/NoteWorkForm.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace NoteWork
+{
+    partial class NoteWorkForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.Location = new System.Drawing.Point(12, 12);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(441, 27);
+            this.descriptionTextBox.TabIndex = 0;
+            // 
+            // NoteWorkForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(467, 54);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Name = "NoteWorkForm";
+            this.Text = "What are you doing/did you do today?";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox descriptionTextBox;
+    }
+}
---
+++ NoteWork/NoteWorkForm.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Configuration;
+using System.Globalization;
+
+namespace NoteWork;
+
+public partial class NoteWorkForm : Form
+{
+    public static NoteWorkForm Create()
+    {
+        Extensions.GetDoneFile();
+
+        return new NoteWorkForm();
+    }
+    private NoteWorkForm()
+    {
+        InitializeComponent();
+        this.descriptionTextBox.KeyPress += this.onKeyPress!;
+
+    }
+
+    private void onKeyPress(object sender, KeyPressEventArgs e)
+    {
+        if (e.KeyChar == (char)Keys.Return)
+        {
+            e.Handled = true;
+            NoteWork();
+        }
+        else if (e.KeyChar == (char)Keys.Escape)
+        {
+            e.Handled = true;
+            Cancel();
+        }
+    }
+
+    public void NoteWork()
+    {
+        string description = this.Description;
+        if (string.IsNullOrEmpty(description))
+            return;
+
+        this.Persist();
+        this.Close();
+    }
+    public void Cancel()
+    {
+        this.Close();
+    }
+
+    public string Description
+    {
+        get => this.descriptionTextBox.Text.Trim();
+    }
+
+    private void Persist()
+    {
+        string todayHeader = $"### {DateTime.Now.ToString("MMMM-dd", CultureInfo.InvariantCulture)}";
+
+        var donePath = Extensions.GetDoneFile();
+
+        var lines = File.ReadAllLines(donePath)
+                       .ToInsertionList(GetFirstLineAfterHeader);
+
+        bool appendHeader = !lines.Any(IsHeader);
+        if (appendHeader)
+        {
+            lines.Add("");
+            lines.Add(todayHeader);
+        }
+        lines.Add($"- {this.Description}");
+        File.WriteAllLines(donePath, lines);
+
+
+        int GetFirstLineAfterHeader(IReadOnlyList<string> existingLines)
+        {
+            bool foundHeader = false;
+            foreach (var (line, index) in existingLines.Select((value, index) => (value, index)))
+            {
+                if (IsHeader(line))
+                {
+                    foundHeader = true;
+                }
+                else if (foundHeader && IsEmptyLine(line))
+                {
+                    return index;
+                }
+            }
+            return existingLines.Count;
+        }
+
+        bool IsHeader(string line)
+        {
+            return line.StartsWith(todayHeader);
+        }
+        static bool IsEmptyLine(string line)
+        {
+            return string.IsNullOrWhiteSpace(line)
+                || line.StartsWith('-') && string.IsNullOrWhiteSpace(line[1..]);
+        }
+    }
+}
+
+
+
+static class Extensions
+{
+    public static string GetDoneFile()
+    {
+        string? value = ConfigurationManager.AppSettings.Get("DoneFile");
+        if (value == null)
+        {
+            throw new ConfigurationErrorsException("Configuration 'DoneFile' not found in app.config");
+        }
+        if (!Path.IsPathFullyQualified(value))
+        {
+            throw new ConfigurationErrorsException("Configuration 'DoneFile' must be a full path");
+        }
+
+        var result = Path.GetFullPath(value);
+        return result;
+    }
+    public static InsertionList<T> ToInsertionList<T>(this IEnumerable<T> sequence, Func<IReadOnlyList<T>, int> computeInsertionIndex)
+    {
+        var list = sequence.ToList();
+        var insertionIndex = computeInsertionIndex(list);
+        return new InsertionList<T>(list, insertionIndex);
+    }
+    public static InsertionList<T> ToInsertionList<T>(this IEnumerable<T> sequence, int insertionIndex)
+    {
+        return new InsertionList<T>(sequence.ToList(), insertionIndex);
+    }
+}
+
+class InsertionList<T> : IEnumerable<T>
+{
+    private readonly List<T> list;
+    private readonly int insertionIndex;
+    private int itemsInsertedCount = 0;
+
+    public InsertionList(List<T> list, int insertionIndex)
+    {
+        if (insertionIndex > list.Count)
+            throw new ArgumentOutOfRangeException(nameof(insertionIndex));
+
+        this.list = list;
+        this.insertionIndex = insertionIndex;
+    }
+    public void Add(T item)
+    {
+        this.list.Insert(insertionIndex + itemsInsertedCount, item);
+        this.itemsInsertedCount++;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return this.list.GetEnumerator();
+    }
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+}
