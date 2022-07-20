d1c815be3d194d75acf0b4ff2b4d535b41134281
Jeroen Bos <15310631+JeroenBos@users.noreply.github.com>
2022-07-20 16:35:34 +0200

    Initial ToggleTouchpad version (requiring state to be passed in)

---
+++ ToggleTouchpad/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+
+public static class ConfigurationExtensions
+{
+    public static (Guid DeviceGuid, string InstancePath) GetConfig()
+    {
+        return (Guid.Parse("745a17a0-74d3-11d0-b6fe-00a0c90f57da"), @"HID\SYNHIDMINI&COL02\1&B12C6D1&2&0001");
+
+        string ? deviceGuid_s = ConfigurationManager.AppSettings.Get("deviceGuid");
+        if (deviceGuid_s == null)
+        {
+            throw new ConfigurationErrorsException("Configuration 'deviceGuid' not found in app.config");
+        }
+        var deviceGuid = Guid.Parse(deviceGuid_s);
+
+
+
+        // get this from the properties dialog box of this device in Device Manager
+
+        string? instancePath = ConfigurationManager.AppSettings.Get("instancePath");
+        if (instancePath == null)
+        {
+            throw new ConfigurationErrorsException("Configuration 'instancePath' not found in app.config");
+        }
+
+        return (deviceGuid, instancePath);
+    }
+}
---
+++ ToggleTouchpad/Program.cs
@@ -0,0 +1,27 @@
+﻿using DisableDevice;
+
+bool enable;
+if (args.Length == 0)
+{
+    Console.WriteLine("Toggling not supported");
+    return 1;
+}
+
+else if (args.Length != 1)
+{
+    Console.WriteLine("Expected 1 argument, true or false");
+    return 1;
+}
+else
+{
+    enable = bool.Parse(args[0]);
+}
+
+
+var (deviceGuid, instancePath) = ConfigurationExtensions.GetConfig();
+
+DeviceHelper.SetDeviceEnabled(deviceGuid, instancePath, enable);
+
+return 0;
+
+
